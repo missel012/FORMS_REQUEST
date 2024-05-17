dd7e2bd448b0902fef8653c1a9ee2b74128c98ee
Esther0212 <eblacas.estherbeuthel@gmail.com>
2024-05-17 23:34:32 +0800

    Forms Request

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\FormRequest;
+
+class FormRequestController extends Controller
+{
+    // Fetch all form requests
+    public function index()
+    {
+        
+    }
+
+    // Store a new form request
+    public function store(Request $request)
+    {
+        
+    }
+
+    // Show a specific form request
+    public function show($id)
+    {
+        
+    }
+
+    // Update an existing form request
+    public function update(Request $request, $id)
+    {
+        
+    }
+
+    // Delete a form request
+    public function destroy($id)
+    {
+        
+    }
+}
